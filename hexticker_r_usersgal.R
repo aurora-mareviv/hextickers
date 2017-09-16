727836242a87300003e30dba2273c3bd01bc200f
aurora-mareviv <mariauror@gmail.com>
2017-09-16 15:45:18 +0200

    Updating stickers

--- hexticker_r_usersgal.R
+++ hexticker_r_usersgal.R
@@ -10,7 +10,7 @@ sticker(imgurl, package="r-users.gal", p_size=8, s_x=1, s_y=.76, s_width=.52,
         h_fill="#d21423", h_color="#1d53b0",
         filename="r_usersgal.png")
 
-imgurl <- paste(wdir, "/rlogo2.png", sep="")
+imgurl <- paste(wdir, "/rlogo3.png", sep="")
 sticker(imgurl, package="r-users.gal", p_size=8, s_x=1, s_y=.76, s_width=.52,
         h_fill="#d21423", h_color="#1d53b0",
-        filename="r_usersgal2.png")
+        filename="rgal17c.png")
