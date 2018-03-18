3ee4dd954f9ae88d169abd6b143a0526b6078ea4
aurora-mareviv <mariauror@gmail.com>
2018-03-18 21:58:37 +0100

    Add new sticker and other improvements

--- hextickers.R
+++ hextickers.R
@@ -10,17 +10,17 @@ wdir <- getwd()
 
 # r-users.gal
 imgurl <- paste(wdir, "/images/rlogo.png", sep="")
-sticker(imgurl, package="r-users.gal", p_size=25, s_x=1, s_y=.76, s_width=.52,
+sticker(imgurl, package="r-users.gal", p_size=30, s_x=1, s_y=.76, s_width=.52,
         h_fill="#d21423", h_color="#1d53b0",
         filename="./stickers/r_usersgal.png")
 
 imgurl <- paste(wdir, "/images/rlogo3.png", sep="")
-sticker(imgurl, package="r-users.gal", p_size=25, s_x=1, s_y=.76, s_width=.52,
+sticker(imgurl, package="r-users.gal", p_size=30, s_x=1, s_y=.76, s_width=.52,
         h_fill="#d21423", h_color="#1d53b0",
         filename="./stickers/rgal17c.png")
 
 # package mthapower
 imgurl <- paste(wdir, "/images/logo-mthapower.png", sep="")
-sticker(imgurl, package="mthapower", p_size=25, s_x=1, s_y=1, s_width=.8,
+sticker(imgurl, package="mthapower", p_size=20, s_x=1, s_y=1, s_width=.8,
         h_fill="#73c46c", h_color="#4ea500",
         filename="./stickers/mthapower.png")
