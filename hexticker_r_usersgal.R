19c380922a12dbce25acef6e3a6da855d8aa2bb4
aurora-mareviv <mariauror@gmail.com>
2018-03-18 19:36:04 +0100

    Add new sticker and other improvements

--- hexticker_r_usersgal.R
+++ hexticker_r_usersgal.R
@@ -1,16 +1,26 @@
+# Install Magick first
+# sudo apt-get install libmagick++-dev
 source("https://bioconductor.org/biocLite.R")
 biocLite("EBImage")
 install.packages("hexSticker")
 library(hexSticker)
 
+
 wdir <- getwd()
 
-imgurl <- paste(wdir, "/rlogo.png", sep="")
+# r-users.gal
+imgurl <- paste(wdir, "/images/rlogo.png", sep="")
 sticker(imgurl, package="r-users.gal", p_size=8, s_x=1, s_y=.76, s_width=.52,
         h_fill="#d21423", h_color="#1d53b0",
-        filename="r_usersgal.png")
+        filename="./stickers/r_usersgal.png")
 
-imgurl <- paste(wdir, "/rlogo3.png", sep="")
+imgurl <- paste(wdir, "/images/rlogo3.png", sep="")
 sticker(imgurl, package="r-users.gal", p_size=8, s_x=1, s_y=.76, s_width=.52,
         h_fill="#d21423", h_color="#1d53b0",
-        filename="rgal17c.png")
+        filename="./stickers/rgal17c.png")
+
+# package mthapower
+imgurl <- paste(wdir, "/images/logo-mthapower.png", sep="")
+sticker(imgurl, package="mthapower", p_size=20, s_x=1, s_y=1, s_width=.8,
+        h_fill="#73c46c", h_color="#4ea500",
+        filename="./stickers/mthapower.png")
